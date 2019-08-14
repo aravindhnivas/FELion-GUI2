a44143fcc928e7919538fbadc49522229d7c9bf1
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2019-08-14 10:39:06 +0200

    added (resolve, reject) Promised by replacing async function

--- modules.js
+++ modules.js
@@ -4,32 +4,35 @@ const { remote } = require('electron');
 const dialog = remote.dialog;
 const mainWindow = remote.getCurrentWindow();
 
-exports.openfiles = async function(title, fileTypeName, fileExtensions) {
+exports.openfiles = function(title, fileTypeName, fileExtensions) {
 
-    console.log("[modules]: Sending files");
-    let send_obj;
-    
-    const options = {
+    return new Promise((resolve, reject) => {
+
+        console.log("[modules]: Sending files");
+        let send_obj;
         
-        title: title,
-        filters: [
-            { name: fileTypeName, extensions: fileExtensions },
-            { name: 'All Files', extensions: ['*'] }
-        ],
-        properties: ['openFile', 'multiSelections'],
-    };
-    
-    await dialog.showOpenDialog(mainWindow, options).then(files => {
-        send_obj = {
-            files: files.filePaths,
-            location: path.dirname(files.filePaths[0])
-        }
+        const options = {
 
-    }).catch(error => {
-        console.log("[modules]: Error occured");
-        return Promise.reject(new Error(error))
+            title: title,
+            filters: [
+                { name: fileTypeName, extensions: fileExtensions },
+                { name: 'All Files', extensions: ['*'] }
+            ],
+            properties: ['openFile', 'multiSelections'],
+        };
+        
+        dialog.showOpenDialog(mainWindow, options).then(files => {
+            send_obj = {
+                files: files.filePaths,
+                location: path.dirname(files.filePaths[0])
+            }
+            console.log("[modules]: File sent succesfully", send_obj);
+            resolve(send_obj);
+
+        }).catch(error => {
+            console.log("[modules]: Error occured");
+            reject(new Error(error));
+            
+        })
     })
-
-    console.log("[modules]: File sent succesfully", send_obj);
-    return Promise.resolve(send_obj)
 }
